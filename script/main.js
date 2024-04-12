242dcc4b0cfcd84fc3b698d0394c375de31b468b
Julien Desrumaux <julien.desrumaux.etu@univ-lille.fr>
2024-04-12 10:51:49 +0200

    Update image size in realisations.html

--- script/main.js
+++ script/main.js
@@ -12,14 +12,14 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-// Déclare une variable pour suivre l'index de la formation actuellement affichée
+
 let currentIndex = 0;
 
-// Fonction pour afficher la formation précédente
+
 function prevFormation() {
     currentIndex--;
 
-    // Si on est en dehors des limites, revenir à la dernière formation
+
     if (currentIndex < 0) {
         currentIndex = document.querySelectorAll('.formation').length - 1;
     }
@@ -27,11 +27,11 @@ function prevFormation() {
     updateDisplay();
 }
 
-// Fonction pour afficher la formation suivante
+
+
 function nextFormation() {
     currentIndex++;
 
-    // Si on est en dehors des limites, revenir à la première formation
     if (currentIndex >= document.querySelectorAll('.formation').length) {
         currentIndex = 0;
     }
@@ -39,17 +39,14 @@ function nextFormation() {
     updateDisplay();
 }
 
-// Fonction pour mettre à jour l'affichage en fonction de l'index actuel
+
 function updateDisplay() {
-    // Récupère toutes les formations
     const formations = document.querySelectorAll('.formation');
 
-    // Cache toutes les formations
     formations.forEach((formation) => {
         formation.style.display = 'none';
     });
 
-    // Affiche la formation actuelle
     formations[currentIndex].style.display = 'block';
 }
 
